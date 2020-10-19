4974561911c8a91c0ee0f64a1f60505d06613d0b
Mamata4225 <72901730+Mamata4225@users.noreply.github.com>
2020-10-19 15:30:50 +0530

    Add files via upload

---
+++ FlowerDemo.java
@@ -0,0 +1,21 @@
+class FlowerDemo{
+
+public static void main(String args[]){
+
+Flower rose = new Flower();
+  rose.name = "rose";
+  rose.color = "red";
+  rose.blooming();
+ 
+Flower Lotus = new Flower();
+  Lotus.name = "Lotus";
+  Lotus.color = "pink";
+  Lotus.blooming();
+
+Flower Hybiscus = new Flower();
+  Hybiscus.name = "Hybiscus";
+  Hybiscus.color = "white";
+  Hybiscus.blooming();
+}
+
+}
