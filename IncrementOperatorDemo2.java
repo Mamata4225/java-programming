2f1afdda004060b3e9f90fa44bb7606a2a5c22a6
Mamata4225 <72901730+Mamata4225@users.noreply.github.com>
2020-10-22 21:27:16 +0530

    Add files via upload

---
+++ IncrementOperatorDemo2.java
@@ -0,0 +1,16 @@
+class IncrementOperatorDemo2{
+
+public static void main(String args[]){
+
+int x=23;
+int y=26;
+int z=++y;
+System.out.println(z); // 
+System.out.println(y++); // 
+System.out.println(x++); // 
+System.out.println(++z); //  
+System.out.println(y); // 
+System.out.println(++x); // 
+System.out.println(++z); // 
+}
+}
