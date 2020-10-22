d03b58285b3996333186f9b6066f6fe6eb5556da
Mamata4225 <72901730+Mamata4225@users.noreply.github.com>
2020-10-22 21:24:41 +0530

    Add files via upload

---
+++ IncDecOperator.java
@@ -0,0 +1,15 @@
+class IncDecOperator{
+
+public static void main(String args[]){
+
+int x=8;
+int y=5;
+int z=--y; // 4
+System.out.println(++y); // 5
+System.out.println(x--); // 8
+System.out.println(z); // 4
+System.out.println(x++); // 7    
+System.out.println(y++); //  5
+System.out.println(y); // 6
+}
+}
