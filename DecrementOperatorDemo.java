7bfe4e8b2c44f7230f80db1256bb4b1578d458d7
Mamata4225 <72901730+Mamata4225@users.noreply.github.com>
2020-10-22 21:25:41 +0530

    Add files via upload

---
+++ DecrementOperatorDemo.java
@@ -0,0 +1,15 @@
+class DecrementOperatorDemo{
+
+public static void main(String args[]){
+
+int x=59;
+int y=29;
+int z=--y;
+System.out.println(--y); // 
+System.out.println(x--); // 
+System.out.println(y--); // 
+System.out.println(--x); //   
+System.out.println(y); // 
+System.out.println(z); // 
+}
+}
