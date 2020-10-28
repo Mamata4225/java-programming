3c0e7c39e219df931ca8c6f1298325c4e433e5b1
Mamata4225 <72901730+Mamata4225@users.noreply.github.com>
2020-10-28 19:16:22 +0530

    Add files via upload

---
+++ BreakStatementDemo.java
@@ -0,0 +1,14 @@
+class BreakStatementDemo{
+
+public static void main(String args[]){
+
+for( int i=0; i<5; i++ ){
+  if( i==3 ){
+ 
+System.out.println("breaking the loop...at 3");
+break;
+}
+System.out.println(i);
+}
+}
+}
